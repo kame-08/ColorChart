e15ca1259d8588fa858582fcfad08f5f5f3d77af
Kame2021 <21cm0108@jec.ac.jp>
2022-08-17 00:39:57 +0900

    Initial Commit

---
+++ ColorChar/ColorCharApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorCharApp.swift
+//  ColorChar
+//
+//  Created by Ryo on 2022/08/17.
+//
+
+import SwiftUI
+
+@main
+struct ColorCharApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorChar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorChar
+//
+//  Created by Ryo on 2022/08/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
