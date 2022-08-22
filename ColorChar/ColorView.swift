c367af7973ce2c98dbf914afa4fa36824e25ec5c
Ryo <21cm0108@jec.ac.jp>
2022-08-22 13:33:15 +0900

    Add:ColorNameやRGB値を長押ししてコピーする際に"コピーしました!"と表示されるようになりました。

--- ColorChar/ColorView.swift
+++ ColorChar/ColorView.swift
@@ -16,71 +16,90 @@ struct ColorView: View {
     @State var colorName :String
     @State var color :Color
     
+     @State var flag = false
+    
     var body: some View {
+        ZStack {
             VStack {
-                HStack {
-                    Text(colorName)
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .lineLimit(1)
-                        .minimumScaleFactor(0.1)
+                    HStack {
+                        Text(colorName)
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .lineLimit(1)
+                            .minimumScaleFactor(0.1)
+                        
+                        Spacer()
+                        
+                    }
+                    .padding(/*@START_MENU_TOKEN@*/[.leading, .bottom, .trailing]/*@END_MENU_TOKEN@*/)
+                    .onLongPressGesture {
+                        UINotificationFeedbackGenerator().notificationOccurred(.success)
+                        UIPasteboard.general.string = ("\(colorName)")
+                        
+                        flag = true
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            flag = false
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    VStack (alignment: .leading) {
+                        if viewModel.RGB.white == nil {
+                            Text("red: \(viewModel.RGB.red!)")
+                            Text("green: \(viewModel.RGB.green!)")
+                            Text("blue: \(viewModel.RGB.blue!)")
+                            Text("opacity: \(viewModel.RGB.opacity)")
+                        } else{
+                            Text("white: \(viewModel.RGB.white!)")
+                            Text("opacity: \(viewModel.RGB.opacity)")
+                        }
+                    }.font(.title)
+                        .onLongPressGesture {
+                            var pasteboard = ""
+                            
+                            UINotificationFeedbackGenerator().notificationOccurred(.success)
+                            
+                            if viewModel.RGB.white == nil {
+                                pasteboard = ("red: \(round(viewModel.RGB.red! * 100) / 100), green: \(round(viewModel.RGB.green! * 100) / 100), blue: \(round(viewModel.RGB.blue! * 100) / 100), opacity: \(round(viewModel.RGB.opacity * 100) / 100)")
+                            } else{
+                                pasteboard = ("white: \(round(viewModel.RGB.white! * 100) / 100), opacity: \(round(viewModel.RGB.opacity * 100) / 100)")
+                            }
+                            UIPasteboard.general.string = pasteboard
+                            
+                            flag = true
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                flag = false
+                            }
+                        }
+                    
+                    Spacer()
+                    
+                    ZStack{
+                        Text(colorScheme == .light ? "黒文字" : "白文字")
+                            .font(.title)
+                            .aspectRatio(contentMode: .fit)
+                        Image(systemName: "squareshape.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .foregroundColor(color)
+                            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    }
                     
                     Spacer()
                     
                 }
-                .padding(/*@START_MENU_TOKEN@*/[.leading, .bottom, .trailing]/*@END_MENU_TOKEN@*/)
-                .onLongPressGesture {
-                    UINotificationFeedbackGenerator().notificationOccurred(.success)
-                    UIPasteboard.general.string = ("\(colorName)")
-                }
-                
-                Spacer()
-                
-                VStack (alignment: .leading) {
-                    if viewModel.RGB.white == nil {
-                        Text("red: \(viewModel.RGB.red!)")
-                        Text("green: \(viewModel.RGB.green!)")
-                        Text("blue: \(viewModel.RGB.blue!)")
-                        Text("opacity: \(viewModel.RGB.opacity)")
-                    } else{
-                        Text("white: \(viewModel.RGB.white!)")
-                        Text("opacity: \(viewModel.RGB.opacity)")
-                    }
-                }.font(.title)
-                    .onLongPressGesture {
-                        var pasteboard = ""
-                        
-                        UINotificationFeedbackGenerator().notificationOccurred(.success)
-                        
-                        if viewModel.RGB.white == nil {
-                            pasteboard = ("red: \(round(viewModel.RGB.red! * 100) / 100), green: \(round(viewModel.RGB.green! * 100) / 100), blue: \(round(viewModel.RGB.blue! * 100) / 100), opacity: \(round(viewModel.RGB.opacity * 100) / 100)")
-                        } else{
-                            pasteboard = ("white: \(round(viewModel.RGB.white! * 100) / 100), opacity: \(round(viewModel.RGB.opacity * 100) / 100)")
-                        }
-                        UIPasteboard.general.string = pasteboard
-                    }
-                
-                Spacer()
-                
-                ZStack{
-                    Text(colorScheme == .light ? "黒文字" : "白文字")
-                        .font(.title)
-                        .aspectRatio(contentMode: .fit)
-                    Image(systemName: "squareshape.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundColor(color)
-                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                }
-                
-                Spacer()
-                
-            }
-        .onChange(of: colorScheme, perform: { newValue in
-            viewModel.getColorElements(color: UIColor(color))
-        })
-        .onAppear() {
-            viewModel.getColorElements(color: UIColor(color))
+            .onChange(of: colorScheme, perform: { newValue in
+                viewModel.getColorElements(color: UIColor(color))
+            })
+            .onAppear() {
+                viewModel.getColorElements(color: UIColor(color))
+        }
+            
+
+                CopyView()
+                .offset(y: flag ? 0 : 100)
+                .animation(.default, value: flag)
         }
     }
 }
